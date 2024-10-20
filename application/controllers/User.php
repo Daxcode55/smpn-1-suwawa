6f91773f5719aa879dd3a3f60344a3d4c4e252b4
unknown <daxcode198@gmail.com>
2024-10-21 03:38:31 +0800

    debugging

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
+	function __construct() {
+        parent::__construct(); //
+    }
+
 	function index()
 	{
 		$this->load->view('public/components/mainHeader');
